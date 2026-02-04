d076721c3bd87045edd582e69ed0e417b9e2a9bc
Taghreed M. Kamel <taghreedMKamel@gmail.com>
2026-02-04 15:21:43 +0200

    Add logout and refresh APIs

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
 
     Route::middleware('auth:api')->group(function () {
-
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
     });
 });
